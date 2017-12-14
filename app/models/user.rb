bff257b2a395f71a8c32d583c19674f9b8d137e9
lsecrease <lsecrease@gmail.com>
2017-12-14 15:52:35 -0600

    Add remember digest and sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
  
+ before_create :create_remember_digest
+ 
  has_secure_password
+ 
+ 
+  def create_remember_digest
+    self.remember_digest = SecureRandom.urlsafe_base64
+    update_attribute(:remember_digest, Digest::SHA1.hexdigest(remember_digest.to_s))
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214183455) do
+ActiveRecord::Schema.define(version: 20171214214241) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171214183455) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
 end
